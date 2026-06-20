dc22cbb96357edba1fc5fe92df3637031720e5ee
vijaymistri1990 <vijaymakwana12790@gmail.com>
2026-06-20 22:15:00 +0530

    adds vercel analytics in code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -55,7 +56,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-white text-gray-800">{children}</body>
+      <body className="min-h-full flex flex-col bg-white text-gray-800">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
